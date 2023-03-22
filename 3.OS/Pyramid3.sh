f219499604f4671c12956535be14a1686fb9cdd2
KamalBaria <97674214+KamalBaria@users.noreply.github.com>
2023-03-22 21:28:57 +0530

    Create Pyramid3.sh

---
+++ 3.OS/Pyramid3.sh
@@ -0,0 +1,39 @@
+# Write a shell script to show the pyramid as
+
+clear
+i=1
+while test $i -le 9
+do
+sp=1
+while test $sp -le `expr 9 - $i`
+do
+echo -n " "
+sp=`expr $sp + 1`
+done
+j=0
+while test $j -ne `expr 2 \* $i - 1`
+do
+echo -n "* "
+j=`expr $j + 1`
+done
+echo
+i=`expr $i + 1`
+done
+i=9
+while test $i -gt 0
+do
+sp=1
+while test $sp -le `expr 9 - $i`
+do
+echo -n " "
+sp=`expr $sp + 1`
+done
+j=0
+while test $j -ne `expr 2 \* $i - 1`
+do
+echo -n "* "
+j=`expr $j + 1`
+done
+echo
+i=`expr $i - 1`
+done
